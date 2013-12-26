ffab9121193868e8eae574cf7b194d83dd62b116
Kamil Waheed <kamilwaheed@hotmail.com>
2013-12-27 00:31:25 +0500

    No changes
    This commit actually has no changes. The client won’t let me sync
    unless I commit this.

--- bean-social.php
+++ bean-social.php
@@ -6,8 +6,8 @@
  * Version: 1.2.1
  * Author: ThemeBeans
  * Author URI: http://themebeans.com/?ref=plugin_bean_social
- *  
- *   
+ *
+ *
  * @package Bean Plugins
  * @subpackage BeanSocial
  * @author ThemeBeans
@@ -29,7 +29,7 @@ if ( !function_exists( 'add_action' ) ) {
 /* PLUGIN UPDATER
 /*===================================================================*/
 //CONSTANTS
-define( 'BEANSOCIAL_EDD_TB_URL', 'http://themebeans.com' ); 
+define( 'BEANSOCIAL_EDD_TB_URL', 'http://themebeans.com' );
 define( 'BEANSOCIAL_EDD_TB_NAME', 'Bean Social' );
 
 //INCLUDE UPDATER
@@ -38,11 +38,12 @@ if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) {
 }
 
 include( dirname( __FILE__ ) . '/updates/EDD_SL_Setup.php' );
+
 //LICENSE KEY
 $license_key = trim( get_option( 'edd_sample_license_key' ) );
 
 //CURRENT BUILD
-$edd_updater = new EDD_SL_Plugin_Updater( BEANSOCIAL_EDD_TB_URL, __FILE__, array( 
+$edd_updater = new EDD_SL_Plugin_Updater( BEANSOCIAL_EDD_TB_URL, __FILE__, array(
 		'version' 	=> '1.2.1',
 		'license' 	=> $license_key,
 		'item_name' => BEANSOCIAL_EDD_TB_NAME,
@@ -70,7 +71,7 @@ add_shortcode( 'bean_social', array( 'Bean_Social', 'render_shortcodes' ) );
 /*===================================================================*/
 if ( ! class_exists( 'Bean_Social' ) ) :
 	class Bean_Social {
-	
+
 		//ICON ARRAY
 	    public static $social_services = array(
 		    "twitter" => "Twitter",
@@ -107,74 +108,74 @@ if ( ! class_exists( 'Bean_Social' ) ) :
 		    "vine" => "Vine",
 		    "github" => "Github"
 		    );
-	
+
 	    private $screen_id = null;
-	
-	
-	
-	
+
+
+
+
 		/*===================================================================*/
 		/* CONSTRUCT
 		/*===================================================================*/
-	    function __construct() 
+	    function __construct()
 	    {
 	        add_action('init', array( &$this, 'social_init') , 0);
 	        add_filter('the_posts', array( &$this, 'add_style_if_shortcode_being_used' ) );
 	    }
-	
-	
-	
-	
+
+
+
+
 	    /*===================================================================*/
 	    /* PLUGIN INIT
 	    /*===================================================================*/
-	    function social_init() 
+	    function social_init()
 	    {
 	        if (is_admin()) {
 	            add_action( 'admin_init', array( &$this, 'register_settings' ) );
 	            add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
 	        } else {
-	
+
 	        }
 	    }
-	    
-	    
-	    
-		
+
+
+
+
 		/*===================================================================*/
 		/* WHITELIST OPTIONS BY THE PLUGIN
 		/*===================================================================*/
-	    function register_settings() 
+	    function register_settings()
 	    {
 	        foreach(self::$social_services as $social_service_slug => $social_service) {
 	            register_setting( 'bean-social-options', 'bean_social-' . $social_service_slug );
 	        }
-	
+
 	    }
-	
-	
-	
-	
+
+
+
+
 	    /*===================================================================*/
 	    /* ADD THE BEAN SOCIAL MENU LINK
-	    /*===================================================================*/ 
-	    function admin_menu() 
-	    {	
+	    /*===================================================================*/
+	    function admin_menu()
+	    {
 	    	add_options_page(
 	    		__('Bean Social', 'bean'), __('Bean Social', 'bean'), 'manage_options', 'bean_social', array(&$this, 'bean_social_admin_page')
 	    	);
 	    }
-	
-	
-	
-	
+
+
+
+
 		/*===================================================================*/
 		/* ADD ACTION FOR ENQUEUING STYLESHEET IF THE SHORTCODE IS FOUND
-		/*===================================================================*/ 
-	    function add_style_if_shortcode_being_used($posts) 
+		/*===================================================================*/
+	    function add_style_if_shortcode_being_used($posts)
 	    {
 	        if (empty($posts)) return $posts;
-	
+
 	        $shortcode_found = false;
 	        foreach ($posts as $post) {
 	            if (stripos($post->post_content, '[bean_social') !== false) {
@@ -182,11 +183,11 @@ if ( ! class_exists( 'Bean_Social' ) ) :
 	                break;
 	            }
 	        }
-	
+
 	        if ($shortcode_found) {
 	            wp_enqueue_style( 'bean-social-style', plugins_url( 'css/bean-social.css', __FILE__ ) );
 	        }
-	
+
 	        return $posts;
 	    }
 
@@ -195,66 +196,66 @@ if ( ! class_exists( 'Bean_Social' ) ) :
 
 		/*===================================================================*/
 	    /*	READ ATTRIBUTES AND RENDER ICONS
-	    /*===================================================================*/   
-	    function render_shortcodes( $atts ) 
+	    /*===================================================================*/
+	    function render_shortcodes( $atts )
 	    {
 	        return self::draw_social_icons( $atts );
 	    }
-	
+
 
 
 
 	    /*===================================================================*/
 	    /*	OUTPUT SOCIAL ICONS
-	    /*===================================================================*/    
-	    function draw_social_icons( $filter = null ) 
+	    /*===================================================================*/
+	    function draw_social_icons( $filter = null )
 	    {
 	        $return_html_string = "<ul class='bean_social_icons'>";
-	
+
 	        if ( !empty($filter) )
 	        {
 	            $filter_arr = explode( ',', str_replace(' ', '', $filter["icons"] ) );
 	        }
-	
-	        foreach( self::$social_services as $social_service_slug => $social_service ) 
+
+	        foreach( self::$social_services as $social_service_slug => $social_service )
 	        {
 	            $social_link = get_option( 'bean_social-' . $social_service_slug );
 	            if (empty($social_link)) continue;
-	
+
 	            if ( isset($filter_arr) ) {
 	                if ( ! ( in_array( $social_service_slug, $filter_arr ) ) && ! ( in_array( $social_service, $filter_arr ) ) ) {
 	                    continue;
 	                }
 	            }
-	
+
 	            $return_html_string .= "<a title='$social_service' href='" . $social_link . "'>" .
 	                                        "<li class='bean_social_icon bean_social-$social_service_slug'></li>" .
 	                                    "</a>";
 	        }
-	
+
 	        $return_html_string .= '</ul>';
 	        return $return_html_string;
 	    }
-			    
-			    
-			    
+
+
+
 
 		/*===================================================================*/
 		/*	RENDER ADMIN PAGE
-		/*===================================================================*/  
-	    function bean_social_admin_page() 
+		/*===================================================================*/
+	    function bean_social_admin_page()
 	    {
 	    	$license = get_option( 'edd_sample_license_key' );
 	    	$status = get_option( 'edd_sample_license_status' );
 	    	?>
-	
+
 	        <div class="wrap">
 				<h2><?php echo esc_html__('Bean Social Plugin', 'bean'); ?></h2>
 				<p>Create and add social media icons throughout your WordPress install using our Bean Social widget and associated shortcodes. Note that only the URLs you enter will display their relative icons. If you like this plugin, consider checking out our other <a href="http://themebeans.com/plugins/?ref=bean_social" target="blank">Free Plugins</a>, as well as our <a href="http://themebeans.com/themes/?ref=bean_social" target="blank">Premium WordPress Themes</a>. Cheers!</p><br />
-				
+
 				<h4 style="font-size: 15px; font-weight: 600; color: #222; margin-bottom: 10px;"><?php _e('Activate License'); ?></h4>
 				<p>Enter the license key <code style="padding: 1px 5px 2px; background-color: #FFF; border-radius: 2px; font-weight: bold; font-family: 'Open Sans',sans-serif;">BEANPLUGIN</code>, hit Save, then Activate, to turn on the plugin updater. You'll then be able to update this plugin from your Plugins Dashboard when future updates are available.</p>
-	             
+
 	            	<form method="post" action="options.php">
 		            	<?php settings_fields('edd_sample_license'); ?>
 		            	<input id="edd_sample_license_key" name="edd_sample_license_key" type="text" class="regular-text" value="<?php esc_attr_e( $license ); ?>" />
@@ -269,19 +270,19 @@ if ( ! class_exists( 'Bean_Social' ) ) :
 								<span style="color: #DD3D36;"><?php _e('&nbsp;&nbsp;Inactive'); ?></span>
 							<?php } ?>
 	            	</form>
-	            	
+
 	     			<br />
 		         	<br />
-		         	
+
 		            <h4 style="font-size: 15px; font-weight: 600; color: #222; margin-bottom: 10px;">Social Shortcodes</h4>
 		            <p>Use the following shortcodes to implement the social icons throughout your install. The first pulls all the available icons, in which you have entered links. The second is an example of a shortcode that filters specific icons to display.</p>
-		           
+
 		            <p><code>[bean_social]</code><br /><br />
 		            <code>[bean_social icons= "Twitter, Facebook, Dribbble"]</code></p><br />
-		            
+
 		            <h4 style="font-size: 15px; font-weight: 600; color: #222; margin-bottom: 10px;">Profile URLs</h4>
 		            <p>Fill in the fields below with your associated URLs:</p>
-			
+
 	            <form method="post" action="options.php">
 	                <?php settings_fields( 'bean-social-options' ); ?>
 	                <table class="form-table">
@@ -300,11 +301,11 @@ if ( ! class_exists( 'Bean_Social' ) ) :
 	            </form>
 	        </div>
 	        <?php
-	    } //END function bean_social_admin_page() 
-	        
+	    } //END function bean_social_admin_page()
+
 } //END class Bean_Social
 
 new Bean_Social;
 
-endif; 
+endif;
 ?>
