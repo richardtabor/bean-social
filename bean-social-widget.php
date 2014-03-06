2e5c7a9ac83e7014795d69fb936ad0b7fdaea918
kamilwaheed <kamilwaheed@icloud.com>
2014-03-06 15:43:46 +0500

    Added a description text area for the widget

--- bean-social-widget.php
+++ bean-social-widget.php
@@ -5,8 +5,8 @@
  * Description:  Display social media icons based on the Bean Social settings in your dashboard.
  * Author: ThemeBeans
  * Author URI: http://themebeans.com
- *  
- *   
+ *
+ *
  * @package Bean Plugins
  * @subpackage BeanSocial
  * @author ThemeBeans
@@ -21,7 +21,7 @@ function reg_bean_social()
 add_action('init', 'reg_bean_social', 1);
 
 // WIDGET CLASS
-class Bean_Social_Widget extends WP_Widget 
+class Bean_Social_Widget extends WP_Widget
 {
 
 
@@ -29,86 +29,99 @@ class Bean_Social_Widget extends WP_Widget
 	/*===================================================================*/
 	/*	WIDGET SETUP
 	/*===================================================================*/
-	public function __construct() 
+	public function __construct()
 	{
 		parent::__construct(
 	 		'bean_social', // BASE ID
 			'Bean Social (ThemeBeans)', // NAME
 			array( 'description' => __( 'Display social media icons/links based on the Bean Social settings.', 'bean' ), )
 		);
-	
-	
+
+
 	    if ( is_active_widget(false, false, $this->id_base) )
 	        add_action( 'wp_head', array(&$this, 'load_widget_style') );
 	}
-	
-	
-	
-	
+
+
+
+
 	/*===================================================================*/
 	/*	ENQUEUE WIDGET STYLE
-	/*===================================================================*/	
-	public function load_widget_style() 
+	/*===================================================================*/
+	public function load_widget_style()
 	{
 	    wp_enqueue_style( 'bean-social-style', plugin_dir_url(__FILE__) . 'css/bean-social.css', false, '1.0', 'all' );
 	}
-	
-	
-	
-	
+
+
+
+
 	/*===================================================================*/
 	/*	DISPLAY WIDGET
-	/*===================================================================*/	
-	public function widget($args, $instance) 
+	/*===================================================================*/
+	public function widget($args, $instance)
 	{
 	    $title = apply_filters( 'widget_title', $instance['title'] );
-	
+        $desc = $instance['desc'];
+
 	    echo $args['before_widget'];
 	        if ( ! empty( $title ) )
 	            echo $args['before_title'] . $title . $args['after_title'];
-	
+
+        if($desc != '') : ?><p><?php echo $desc; ?></p><?php endif;
+
 	    echo Bean_Social::draw_social_icons();
-	
+
 	    echo $args['after_widget'];
 	}
-	
-	
-	
-	
+
+
+
+
 	/*===================================================================*/
 	/*	UPDATE WIDGET
 	/*===================================================================*/
-	public function update($new_instance, $old_instance) 
+	public function update($new_instance, $old_instance)
 	{
 	    $instance = array();
-	    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-	
+
+        // STRIP TAGS TO REMOVE HTML - IMPORTANT FOR TEXT IMPUTS
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        $instance['desc'] = stripslashes( $new_instance['desc'] );
+
 	    return $instance;
 	}
-	
-	
-	
-	
+
+
+
+
 	/*===================================================================*/
 	/*	WIDGET SETTINGS (FRONT END PANEL)
-	/*===================================================================*/	 
-	public function form($instance) 
+	/*===================================================================*/
+	public function form($instance)
 	{
-	    if ( isset( $instance[ 'title' ] ) ) {
-	        $title = $instance[ 'title' ];
-	    }
-	    else {
-	        $title = __( 'Bean Social.', 'bean' );
-	    }
+        // WIDGET DEFAULTS
+        $defaults = array(
+            'title' => 'We\'re Social',
+            'desc'  => __( 'Follow us across the internet.', 'bean' )
+        );
+
+        $instance = wp_parse_args( (array) $instance, $defaults );
+
 	    ?>
 		<p>
 		<?php _e('Set your social icon links within the "Bean Social" menu item in your WordPress settings menu.', 'bean'); ?>
 		</p>
-		
+
 		<p>
 		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" />
 		</p>
+
+        <p style="margin-top: -8px;">
+        <textarea class="widefat" rows="5" cols="15" id="<?php echo $this->get_field_id( 'desc' ); ?>" name="<?php echo $this->get_field_name( 'desc' ); ?>"><?php echo $instance['desc']; ?></textarea>
+        </p>
+
 	    <?php
 	}
 
